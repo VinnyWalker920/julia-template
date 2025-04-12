ca7ff8df5ee39dc4e4d0879c72aff440bf3f51b4
Zappa <vinnywalker920@gmail.com>
2025-04-12 00:29:17 -0500

    Intital Webserver Setup

--- src/main.jl
+++ src/main.jl
@@ -1,1 +1,8 @@
-println("result 1: ", 1 + 1)
+using Genie
+
+route("/") do
+  "Hello! This is my tiny Genie website."
+end
+
+Genie.Configuration.config!(server_host = "0.0.0.0", server_port = 8000)
+up(8000, async = false)
